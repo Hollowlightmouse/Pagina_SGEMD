0710c7dc5c10eb1e91d88c8022d1db9e5ae9b2b3
Hollowlightmouse <johanstevenlopezordonez1102@gmail.com>
2025-11-20 11:40:00 -0500

    Back

--- bloc/logic/main.js
+++ bloc/logic/main.js
@@ -1,8 +1,8 @@
 // === EQUIPO ===
 const miembros = [
   { nombre: "Juan Pablo Ángel Quitián", apodo: "backend-dev", descripcion: "Desarrollador backend enfocado en la lógica del sistema y gestión de base de datos.", imagen: "bloc/style/img/Juan_Pablo.jpg" },
-  { nombre: "Joel Sebastián Bueno Medina", apodo: "backend-dev", descripcion: "Desarrollador backend enfocado en la lógica del sistema y gestión de base de datos.", imagen: "bloc/style/img/Joel_Malo.jpg" },
-  { nombre: "Johan Steven López Ordoñez", apodo: "frontend-dev", descripcion: "Diseñador y desarrollador frontend responsable de la interfaz web.", imagen: "bloc/style/img/Johan_Sanchez.jpg" },
+  { nombre: "Joel Sebastián Bueno Medina", apodo: "frontend-dev", descripcion: "Diseñador y desarrollador frontend responsable de la interfaz web.", imagen: "bloc/style/img/Joel_Malo.jpg" },
+  { nombre: "Johan Steven López Ordoñez", apodo: "backend-dev", descripcion: "Desarrollador backend enfocado en la lógica del sistema y gestión de base de datos.", imagen: "bloc/style/img/Johan_Sanchez.jpg" },
   { nombre: "Juan Sebastián Rueda Ortiz", apodo: "frontend-dev", descripcion: "Diseñador y desarrollador frontend responsable de la interfaz web.", imagen: "bloc/style/img/Juan_Rueda.jpg" },
   { nombre: "Jairo Esteban Salgado Tinoco", apodo: "Tech Writer", descripcion: "Responsable de la creación, organización y mantenimiento de la documentación del proyecto SGEMD.", imagen: "bloc/style/img/Jairo_Tinoco.jpg" },
   { nombre: "Esteban Morales", apodo: "scrum-master", descripcion: "Asesor área de programación y mentor académico del proyecto SGEMD.", imagen: "bloc/style/img/Esteban_Morales.jpg" },
